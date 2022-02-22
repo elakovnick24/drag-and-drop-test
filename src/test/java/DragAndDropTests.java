adf381d5629247278c03383dd8e68ad2ad0f7fb3
nelakov <ne@moeco.io>
2022-02-23 01:18:16 +0300

    add tests

---
+++ src/test/java/DragAndDropTests.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver
+
+public class DragAndDropTests {
+    @Test
+    public void shouldBeSwapped() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("column-b");
+        $("#column-a").$("header").shouldHave(text("B"));
+        $("#column-b").$("header").shouldHave(text("A"));
+    }
+}
